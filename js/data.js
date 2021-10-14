336662cdf4d4e45e966d7036bc53de19e6ede2fd
lizhenhu <2740128806@qq.com>
2021-10-14 23:40:59 +0800

    Initial project

---
+++ js/data.js
@@ -0,0 +1,103 @@
+
+window.file_groupss = [
+  {
+    type: 'image',
+    title: '图片',
+    path: './files/images/',
+    files: [
+      {
+        name: '5803.jpg',
+        size: 5803,
+      },
+      {
+        name: '6136.jpg',
+        size: 6136,
+      },
+      {
+        name: '145.jpg',
+        size: 145,
+      },
+    ],
+  },
+  {
+    type: 'video',
+    title: '视频',
+    path: './files/videos/',
+    files: [
+      {
+        name: '1830.mp4',
+        size: 1830,
+      },
+      {
+        name: '4270.mp4',
+        size: 4270,
+      },
+      {
+        name: '5382.mp4',
+        size: 5382,
+      },
+      {
+        name: '5984.mp4',
+        size: 5984,
+      },
+      {
+        name: '6817.mp4',
+        size: 6817,
+      },
+      {
+        name: '8110.mp4',
+        size: 8110,
+      },
+      {
+        name: '771.mp4',
+        size: 771,
+      },
+    ],
+  },
+  {
+    type: 'audio',
+    title: '音频',
+    path: './files/audios/',
+    files: [
+      {
+        name: '109.mp3',
+        size: 109,
+      },
+      {
+        name: '180.mp3',
+        size: 180,
+      },
+      {
+        name: '440Hz-5sec.mp3',
+        size: 28,
+      },
+      {
+        name: '1000Hz-5sec.mp3',
+        size: 28,
+      },
+    ],
+  },
+  {
+    type: 'document',
+    title: '文档',
+    path: './files/documents/',
+    files: [
+      {
+        name: 'text.txt',
+        size: 1,
+      },
+      {
+        name: 'json.json',
+        size: 1,
+      },
+      {
+        name: 'pdf.pdf',
+        size: 34,
+      },
+      {
+        name: 'docx.docx',
+        size: 14,
+      },
+    ],
+  },
+]
